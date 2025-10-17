401608c3eae93d03353497f7038f44cfbe39ae33
steve0557-blip <youv0557@gmail.com>
2025-10-17 18:39:36 +0800

    Uploaded Java case problem and output screenshot

---
+++ harayo/CaseProblem.java
@@ -0,0 +1,17 @@
+public class CaseProblem {
+ 
+    public static void main(String[] args) {
+        
+        System.out.println("Name: Steven S. Harayo ");
+        System.out.println("ID Number: 2024304176 ");
+        System.out.println("------------------------------");
+
+        
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        
+        System.out.println("Result: The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
